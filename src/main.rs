343c3f094a6ab5fccaee0986e46258d830289872
Hyeon Kim <simnalamburt@gmail.com>
2020-12-28 13:53:18 +0900

    Use once_cell instead of lazy_static
    Closes #1

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 use hyper::{body::to_bytes, client::HttpConnector, Body, Client, Request};
 use hyper_rustls::HttpsConnector;
-use lazy_static::lazy_static;
 use netlify_lambda::{lambda, Context};
+use once_cell::sync::OnceCell;
 use serde_json::{json, Value};
 use std::{env, error::Error};
 
 type LambdaError = Box<dyn Error + Sync + Send + 'static>;
 
-lazy_static! {
-    static ref WEBHOOK_TOKEN: String = env::var("WEBHOOK_TOKEN").unwrap();
-    static ref CLIENT: Client<HttpsConnector<HttpConnector>> =
-        Client::builder().build(HttpsConnector::with_native_roots());
-}
+static WEBHOOK_TOKEN: OnceCell<String> = OnceCell::new();
+static CLIENT: OnceCell<Client<HttpsConnector<HttpConnector>>> = OnceCell::new();
 
 #[lambda]
 #[tokio::main]
 async fn main(event: Value, _context: Context) -> Result<Value, LambdaError> {
+    let webhook_token = WEBHOOK_TOKEN.get_or_try_init(|| env::var("WEBHOOK_TOKEN"))?;
+    let client =
+        CLIENT.get_or_init(|| Client::builder().build(HttpsConnector::with_native_roots()));
+
     let message = &event["Records"][0]["Sns"]["Message"];
 
     let payload = json!({
@@ -29,11 +30,11 @@ async fn main(event: Value, _context: Context) -> Result<Value, LambdaError> {
         .method("POST")
         .uri(format!(
             "https://discord.com/api/webhooks/792425523639746611/{}",
-            *WEBHOOK_TOKEN
+            webhook_token
         ))
         .header("Content-Type", "application/json")
         .body(Body::from(payload.to_string()))?;
-    let (parts, body) = CLIENT.request(req).await?.into_parts();
+    let (parts, body) = client.request(req).await?.into_parts();
 
     println!(
         "{}",
